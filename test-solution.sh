5ac00aa93efe552b59673c5098817e84d289fbff
Mauricio Panelo <mpanelo@ucsd.edu>
2017-10-04 16:14:15 -0700

    finished comments

--- test-solution.sh
+++ test-solution.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-
 make clean
 make 
 
-MYSAMPLES=mysamples
-mkdir -p ${MYSAMPLES}
+if [[ "$#" -ne "2" ]] && [[ "$#" -ne "1" ]]; then
+	echo "Miles Eli Jones rules."
+	exit 1
+fi
 
-for file in $(ls samples/*.java); do
-	filename=$(echo $file | cut -f1 -d '.' | cut -f2 -d '/')
-	./lexer < $file > ${MYSAMPLES}/$filename.myout
-	diff ${MYSAMPLES}/$filename.myout samples/$filename.out
-done
+if [[ "$#" -eq "2" ]]; then 
+	./lexer < $1 > lexer.output
+	diff lexer.output $2
+elif [[ "$#" -eq "1" ]] && [[ "$1" -eq "ALL" ]]; then
+
+	MYSAMPLES=mysamples
+	mkdir -p ${MYSAMPLES}
+
+	for file in $(ls samples/*.java); do
+		echo $file
+		filename=$(echo $file | cut -f1 -d '.' | cut -f2 -d '/')
+		./lexer < $file > ${MYSAMPLES}/$filename.myout
+		diff ${MYSAMPLES}/$filename.myout samples/$filename.out
+	done
+
+fi
