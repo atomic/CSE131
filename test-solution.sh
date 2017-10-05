535fc1fed97fdb35c976209c28d59e386f90c4e8
Mauricio Panelo <mpanelo@ucsd.edu>
2017-10-05 13:41:00 -0700

    updated testing script

--- test-solution.sh
+++ test-solution.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
+TXT_RESET=$(tput sgr 0 2> /dev/null)
+TXT_RED=$(tput setaf 1 2> /dev/null)
+TXT_GREEN=$(tput setaf 2 2> /dev/null)
+MYSAMPLES=mysamples
+
 make clean
 make 
 
-if [[ "$#" -ne "2" ]] && [[ "$#" -ne "1" ]]; then
-	echo "Miles Eli Jones rules."
+function error_msg() {
+	echo "Miles Eli Jones rules! Also you broke the script."
+	echo "Exiting..."
 	exit 1
-fi
+}
 
-if [[ "$#" -eq "2" ]]; then 
-	./lexer < $1 > lexer.output
-	diff lexer.output $2
-elif [[ "$#" -eq "1" ]] && [[ "$1" -eq "ALL" ]]; then
+function compare() {
+	local samples_dir=samples/
+	./lexer < $samples_dir/$1.java > lexer.output
+	vimdiff lexer.output $samples_dir/$1.out
+	rm lexer.output
+}
 
-	MYSAMPLES=mysamples
+function compare_all() {
 	mkdir -p ${MYSAMPLES}
 
 	for file in $(ls samples/*.java); do
-		echo $file
+
 		filename=$(echo $file | cut -f1 -d '.' | cut -f2 -d '/')
-		./lexer < $file > ${MYSAMPLES}/$filename.myout
-		diff ${MYSAMPLES}/$filename.myout samples/$filename.out
+
+		if [ $filename = "unrecognized_char" ] || [ $filename = "unterminated_comment" ]; then
+			./lexer < $file 2> ${MYSAMPLES}/$filename.myout
+		else
+			./lexer < $file > ${MYSAMPLES}/$filename.myout
+		fi
+
+		if diff -s ${MYSAMPLES}/$filename.myout samples/$filename.out > /dev/null; then
+			echo "${TXT_GREEN}[PASSED]${TXT_RESET} $file"
+		else
+			echo "${TXT_RED}[FAILED]${TXT_RESET} $file"
+		fi
+
 	done
 
-fi
+	rm -rf ${MYSAMPLES}
+}
+
+while true; do
+	case "$1" in
+		-s | --single ) compare $2; break ;;
+		--all ) compare_all; break ;;
+		*     ) error_msg ;;
+	esac
+done
