8c89d8b6abd8e83aa2a6e9accb7d313b15a4a50a
Tony Lim <atomictheorist@gmail.com>
2017-10-30 14:08:55 -0700

    flex lib and add mao\'s tester for pa3

---
+++ PA3/pa3-tester.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+TXT_RESET=$(tput sgr 0 2> /dev/null)
+TXT_RED=$(tput setaf 1 2> /dev/null)
+TXT_GREEN=$(tput setaf 2 2> /dev/null)
+OUTFILES=outfiles
+
+if [ ! -f "parser" ]; then
+    echo "'parser' executable does not exist. Calling make"
+    make clean;
+    make;
+fi
+
+function usage() {
+    echo -e "$0 [OPTIONS]...\n"
+    echo -e "OPTIONS"
+    echo -e "  --all Compares all solution files"
+    exit 1
+}
+
+function compare_all() {
+    mkdir -p ${OUTFILES}
+
+    for file in $(ls samples/*.java); do
+
+        filename=$(echo $file | cut -f1 -d '.' | cut -f2 -d '/')
+        ./parser < $file > ${OUTFILES}/$filename.out 2>&1
+
+        if cmp -s ${OUTFILES}/$filename.out samples/$filename.out; then
+            echo "${TXT_GREEN}[PASSED]${TXT_RESET} $file"
+        else
+            echo "${TXT_RED}[FAILED]${TXT_RESET} $file"
+        fi
+
+    done
+
+    rm -rf ${OUTFILES}
+}
+
+while true; do
+    case "$1" in
+        --all ) compare_all; break ;;
+        *     ) usage ;;
+    esac
+done
