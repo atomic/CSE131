181228bd8baae599ccaf97159d83388d199a1b22
Tony Lim <atomictheorist@gmail.com>
2017-11-15 13:27:48 -0800

    dead code, add sample 5

--- PA5/samples/dead_code_5.java
+++ PA5/samples/dead_code_5.java
@@ -1,15 +1,17 @@
 void main() {
-    int a = 1;
+    int a_used = 1;
     int b_not_used = 0;
     int c_not_used = 0;
     int d_used = 0;
     int e_used = 0;
 
-    if (a < 2) {
-        a++;
+    if (a_used < 2) {
+        a_used++;
     } else {
-        a = a * 3;
+        a_used = a_used * 3;
     }
 
+    int f_not_used = 0;
+    int g_not_used = 0;
     return d_used + e_used;
 }
