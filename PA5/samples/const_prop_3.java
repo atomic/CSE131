60abbf5ad2011e54f000a236e6d4c72dbdd7fa3b
Tony Lim <atomictheorist@gmail.com>
2017-11-15 20:01:56 -0800

    constant propagation pass slides tests

---
+++ PA5/samples/const_prop_3.java
@@ -0,0 +1,8 @@
+void main() {
+    if (true) {
+        int x = 8;
+    } else {
+        int y = 9;
+    }
+    int z = x;
+}
---
+++ PA5/samples/const_prop_4.java
@@ -0,0 +1,8 @@
+void main() {
+    int x = 8;
+    if (true) {
+        int y = x + 1;
+    } else {
+        int y = x + 1;
+    }
+}
